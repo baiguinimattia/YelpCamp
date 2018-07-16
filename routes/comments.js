fce51cef62b49f7dadb0b4db7694ea6886b7da8f
Baiguini Mattia <mattia.baiguini@gmail.com>
2018-07-16 20:40:21 +0300

    Comment authorization improvement

--- routes/comments.js
+++ routes/comments.js
@@ -42,6 +42,66 @@ router.post("/campgrounds/:id/comments" , isLoggedIn , function(req , res){
     });
 });
 
+router.get("/campgrounds/:id/comments/:comment_id/edit" , checkCommentOwnership , function(req , res){
+        Comment.findById(req.params.comment_id , function(error , foundComment){
+                if(error){
+                        res.redirect("back");
+                }
+                else{
+                        res.render("comments/edit" , {campground_id : req.params.id , comment: foundComment});
+                }
+        })
+        
+});
+
+
+router.put("/campgrounds/:id/comments/:comment_id" , checkCommentOwnership , function( req , res){
+        Comment.findByIdAndUpdate( req.params.comment_id , req.body.comment , function(error , updatedComment){
+                if(error){
+                        res.redirect("back");
+                }
+                else{
+                        res.redirect("/campgrounds/" + req.params.id);
+                }
+        })
+});
+
+router.delete("/campgrounds/:id/comments/:comment_id" , checkCommentOwnership , function( req , res){
+        Comment.findByIdAndRemove(req.params.comment_id , function(error){
+                if(error){
+                        console.log(req.params.comment_id);
+                        res.redirect("back");
+                }
+                else{
+                        res.redirect("/campgrounds/" + req.params.id);
+                }
+        });
+});
+
+function checkCommentOwnership( req , res , next){
+        if(req.isAuthenticated()){
+                Comment.findById(req.params.comment_id , function(error , foundComment){
+                        if( error){
+                                console.log(error);
+                                res.redirect("back");
+
+                        }
+                        else{
+                                if(req.user._id.equals(foundComment.author.id)){
+                                        next();
+                                }
+                                else{
+                                        console.log("mars ma nu e al tau");
+                                }
+                        }
+                });
+        }
+        else{
+                res.redirect("back");
+        }
+};
+
+
 function isLoggedIn(req , res , next){
     if(req.isAuthenticated()){
             return next();
